eebe9a36c7a49ba5223c01c03780ace38a734a82
abi4679553 <abivengadajalam7708@gmail.com>
2025-10-08 16:23:41 +0530

    new

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <div >
+            <div className='bg-black'>
+
+                <h1>Welcome to Our Mini Project !!</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home
