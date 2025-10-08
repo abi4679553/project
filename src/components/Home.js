066f04fa8141f980fa647389f2d212a26af8a8a4
abi4679553 <abivengadajalam7708@gmail.com>
2025-10-08 20:57:32 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import React from 'react'
 const Home = () => {
     return (
         <div >
-            <div className='bg-black'>
-
-                <h1>Welcome to Our Mini Project !!</h1>
+            <div className='bg-purple-700 p-5 text-white text-center flex '>
+            <p>ebook : preventing cheatbot cheating in online learning</p>
+            <button className='bg-gray-200 rounded-2xl p-2 text-left'>Access</button>
             </div>
         </div>
     )
