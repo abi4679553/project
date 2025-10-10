c9ca690206e18795a57d1dbeb7702c49576bfe75
abi4679553 <abivengadajalam7708@gmail.com>
2025-10-10 15:36:00 +0530

    about

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,11 @@ import React from 'react'
 const Home = () => {
     return (
         <div >
-            <div className='bg-purple-700 p-5 text-white text-center flex '>
-            <p>ebook : preventing cheatbot cheating in online learning</p>
-            <button className='bg-gray-200 rounded-2xl p-2 text-left'>Access</button>
+            <div className=' bg-purple-700 p-5 text-white text-center flex justify-between '>
+            <p className='ml-52  '>ebook : preventing cheatbot cheating in online learning</p>
+            <button className='bg-gray-200 rounded-3xl p-2 mr-52 px-5 text-black'>Access</button>
             </div>
+            <p>ugugsugsgvgvyg</p>
         </div>
     )
 }
