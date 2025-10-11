4039ded4b75c1b57c9e805ced1d36c263208a572
abi4679553 <abivengadajalam7708@gmail.com>
2025-10-11 16:24:55 +0530

    body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Home from "./components/Home";
+import Header from "./components/Header"; 
+import Home from "./components/Home" ;
+
+
 
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+    <Header />
+    <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,26 +4,11 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <div className=' bg-purple-700 p-5 text-white text-center flex justify-between items-center'>
-            <p className='ml-64 font-bold '>ebook : preventing cheatbot cheating in online learning</p>
-            <button className='bg-gray-200 rounded-3xl p-2 mr-96 px-5 text-black'>Access</button>
-            </div>
-           <div className='flex text-center border bg-white justify-between p-4 '>
-            <div className='flex p-4'>
-            <p className='text-3xl'>Cheat Sense</p>
-            </div>
-            <div className='flex gap-12 mt-5'>
-            <p>Higher Ed</p>
-            <p>Professional Ed</p>
-            <p>Resources</p>
-            <p>Resources</p>
-           </div>
-           <div className='flex gap-5 mt-3 '>
-            <button className='bg-blue-500 rounded-full w-32 h-10 text-white'>Demo</button>
-            <button className='bg-pink-500 rounded-full w-32 h-10 text-white '>support</button>
-           </div>
-           </div>
-
+            <div className='text-center p-10 bg-blue-950 text-white'>
+            <p>Honorlock Online Proctoring</p>
+            <p> A Better Way to Test</p>
+            <p>Integrity Matters. So Do People. Rethink How You Secure Exams.</p>
+        </div>
         </div>
     )
 }
